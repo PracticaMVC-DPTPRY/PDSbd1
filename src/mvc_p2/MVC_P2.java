47ed31f4fad233818bc9f54d14621b029cf1d205
danielxp88 <danielxp@gmail.micorreo.upp.edu.mx>
2022-02-09 23:05:44 -0600

    print en la clase principal prueba

--- src/mvc_p2/MVC_P2.java
+++ src/mvc_p2/MVC_P2.java
@@ -11,7 +11,7 @@ public class MVC_P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Borrame prueba");
     }
     
 }
