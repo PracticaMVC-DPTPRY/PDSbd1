3b27153d1439a7fdc71433e7ce2a6504cf3e9423
danielxp88 <danielxp@gmail.micorreo.upp.edu.mx>
2022-02-15 17:34:20 -0600

    Propuesta para frame principal y clase de controladorMDBTS completa pero sigue con los errores por la falta de la tabla

--- src/Controlador/ControladorMDBTS.java
+++ src/Controlador/ControladorMDBTS.java
@@ -21,7 +21,7 @@ public class ControladorMDBTS implements ActionListener {
     public void actionPerformed(ActionEvent e){
         String leyenda="";
         if(e.getSource()==_view.cmdAlta){
-            leyenda=_model.registrarAlta("seguimiento_clientes"," null,"+_view.txtCedula.getText()+",'"_view.txtLugar_Expedicion.getText() );
+            leyenda=_model.registrarAlta("seguimiento_clientes"," null,"+_view.txtCedula.getText()+",'"+_view.txtLugar_Expedicion.getText()+"','"+_view.txtNombre_Completo.getText()+"','" +_view.txtProfesion.getText()+"','"+_view.txtDireccion_Residencial.getText()+"','"+_view.txtCiudad.getText()+"','"+_view.txtDepartamento.getText()+"','"+_view.txtEmail.getText()+"',"+_view.txtTel_Residencia.getText()+","+_view.txtTel_Trabajo.getText()+","+_view.txtNum_Factura.getText()+",'"+_view.txtTipo_Cartera.getText()+"','"+_view.txtFecha_Mora.getText()+"',"+_view.txtDias_Mora.getText()+","+_view.txtVal_Mora.getText()+","+_view.txtV_seguro.getText()+","+_view.txtInteres_Mora.getText()+","+_view.txtSald_Restante.getText()+",'"+_view.txtTipo_Cobro.getText()+",'"+_view.txtFecha_Acuerdo_Uno.getText()+"','"+_view.txtAcuerdo_Uno.getText()+"','"+_view.txtFecha_Acuerdo_Dos.getText()+"','"+_view.txtAcuerdo_Dos.getText()+"','"+_view.txtFecha_Acuerdo_Tres.getText()+"','"+_view.txtAcuerdo_Tres.getText()+"','"+_view.txtFecha_Acuerdo_Cuatro.getText()+"','"+_view.txtAcuerdo_Cuatro.getText()+"'");
             JOptionPane.showMessageDialog(null, leyenda);
             limpiar();
         }
@@ -32,5 +32,30 @@ public class ControladorMDBTS implements ActionListener {
     public void limpiar(){
        _view.txtCedula.setText(null);
        _view.txtLugar_Expedicion.setText(null);
+       _view.txtNombre_Completo.setText(null);
+       _view.txtProfesion.setText(null);
+       _view.txtDireccion_Residencial.setText(null);
+       _view.txtCiudad.setText(null);
+       _view.txtDepartamento.setText(null);
+       _view.txtEmail.setText(null);
+       _view.txtTel_Residencia.setText(null);
+       _view.txtTel_Trabajo.setText(null)
+       _view.txtNum_Factura.setText(null);
+       _view.txtTipo_Cartera.setText(null);
+       _view.txtFecha_Mora.setText(null);
+       _view.txtDias_Mora.setText(null);
+       _view.txtVal_Mora.setText(null);
+       _view.txtV_seguro.setText(null);
+       _view.txtInteres_Mora.setText(null);
+       _view.txtSald_Restante.setText(null);
+       _view.txtTipo_Cobro.setText(null);
+       _view.txtFecha_Acuerdo_Uno.getText(null);
+       _view.txtAcuerdo_Uno.setText(null);
+       _view.txtFecha_Acuerdo_Dos.setText(null);
+       _view.txtAcuerdo_Dos.setText(null);
+       _view.txtFecha_Acuerdo_Tres.setText(null);
+       _view.txtAcuerdo_Tres.setText(null);
+       _view.txtFecha_Acuerdo_Cuatro.setText(null);
+       _view.txtAcuerdo_Cuatro.setText(null);
     }
 }
--- src/Vista/FrmPrincipal.java
+++ src/Vista/FrmPrincipal.java
@@ -27,21 +27,155 @@ public class FrmPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        cmdPrimeraTabla = new javax.swing.JButton();
+        cmdSegundaTabla = new javax.swing.JButton();
+        cmdTerceraTabla = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel1.setText("UNIVERSIDAD POLITÉCNICA DE PACHUCA ");
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel2.setText("PRACTICA MVC");
+
+        jLabel4.setText("Integrantes:");
+
+        jLabel5.setText("Reyes Sánchez Tanya Jatziri");
+
+        jLabel6.setText("Rodríguez Canales Yasmin");
+
+        jLabel7.setText("Soto Lemus Paola Leilany");
+
+        jLabel8.setText("Téllez Sanchez Rubí Mariana");
+
+        jLabel9.setText("Vargas Islas Pablo Armando");
+
+        jLabel10.setText("Xithe Pintado Daniel");
+
+        jLabel11.setText("Tabla Clientes ");
+
+        jLabel12.setText("Tabla Seguimiento_Clientes ");
+
+        jLabel13.setText("Tabla tbusuarios ");
+
+        cmdPrimeraTabla.setText("Ir");
+        cmdPrimeraTabla.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdPrimeraTablaActionPerformed(evt);
+            }
+        });
+
+        cmdSegundaTabla.setText("Ir");
+        cmdSegundaTabla.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdSegundaTablaActionPerformed(evt);
+            }
+        });
+
+        cmdTerceraTabla.setText("Ir");
+        cmdTerceraTabla.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdTerceraTablaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 565, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(142, 142, 142)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel9)
+                    .addComponent(jLabel10)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(139, 139, 139)
+                        .addComponent(jLabel12)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel13)))
+                .addContainerGap(143, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(92, 92, 92)
+                        .addComponent(jLabel11))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(106, 106, 106)
+                        .addComponent(cmdPrimeraTabla)))
+                .addGap(181, 181, 181)
+                .addComponent(cmdSegundaTabla)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cmdTerceraTabla)
+                .addGap(172, 172, 172))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 339, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel10)
+                .addGap(60, 60, 60)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel11)
+                    .addComponent(jLabel12)
+                    .addComponent(jLabel13))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmdPrimeraTabla)
+                    .addComponent(cmdSegundaTabla)
+                    .addComponent(cmdTerceraTabla))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cmdPrimeraTablaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdPrimeraTablaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmdPrimeraTablaActionPerformed
+
+    private void cmdSegundaTablaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdSegundaTablaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmdSegundaTablaActionPerformed
+
+    private void cmdTerceraTablaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdTerceraTablaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmdTerceraTablaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +213,20 @@ public class FrmPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdPrimeraTabla;
+    private javax.swing.JButton cmdSegundaTabla;
+    private javax.swing.JButton cmdTerceraTabla;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     // End of variables declaration//GEN-END:variables
 }
