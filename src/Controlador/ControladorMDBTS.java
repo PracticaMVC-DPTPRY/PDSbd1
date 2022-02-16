eedf91a098d50091f2d03da75bd7b2c731d6fa28
yasmin <yasminrodriguezc@micorreo.upp.edu.mx>
2022-02-15 23:03:45 -0600

    #1 -> TablaSeguimiento

--- src/Controlador/ControladorMDBTS.java
+++ src/Controlador/ControladorMDBTS.java
@@ -1,5 +1,6 @@
 //Controlador para la tabla seguimiento 
 package Controlador;
+import Vista.FrmSeguiminto2;
 import Vista.FrmSeguimiento;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,55 +8,95 @@ import javax.swing.JOptionPane;
 import pkgModelo.MDB;
 
 public class ControladorMDBTS implements ActionListener {
+    private FrmSeguiminto2 _view2;
     private FrmSeguimiento _view;
     private MDB _model;
+
+    public ControladorMDBTS(FrmSeguiminto2 _view2, FrmSeguimiento _view, MDB _model) {
+        this._view2 = _view2;
+        this._view = _view;
+        this._model = _model;
+        this._view2.cmdAlta.addActionListener(this);
+       this._view2.cmdEliminar.addActionListener(this);
+    }
+
+  
     
-    public ControladorMDBTS(FrmSeguimiento view, MDB model){
-       this._model=model;
-       this._view=view;
-       this._view.cmdAlta.addActionListener(this);
-       this._view.cmdEliminar.addActionListener(this);
-}
+
 
     @Override 
     public void actionPerformed(ActionEvent e){
         String leyenda="";
-        if(e.getSource()==_view.cmdAlta){
-            leyenda=_model.registrarAlta("seguimiento_clientes"," null,"+_view.txtCedula.getText()+",'"+_view.txtLugar_Expedicion.getText()+"','"+_view.txtNombre_Completo.getText()+"','" +_view.txtProfesion.getText()+"','"+_view.txtDireccion_Residencial.getText()+"','"+_view.txtCiudad.getText()+"','"+_view.txtDepartamento.getText()+"','"+_view.txtEmail.getText()+"',"+_view.txtTel_Residencia.getText()+","+_view.txtTel_Trabajo.getText()+","+_view.txtNum_Factura.getText()+",'"+_view.txtTipo_Cartera.getText()+"','"+_view.txtFecha_Mora.getText()+"',"+_view.txtDias_Mora.getText()+","+_view.txtVal_Mora.getText()+","+_view.txtV_seguro.getText()+","+_view.txtInteres_Mora.getText()+","+_view.txtSald_Restante.getText()+",'"+_view.txtTipo_Cobro.getText()+",'"+_view.txtFecha_Acuerdo_Uno.getText()+"','"+_view.txtAcuerdo_Uno.getText()+"','"+_view.txtFecha_Acuerdo_Dos.getText()+"','"+_view.txtAcuerdo_Dos.getText()+"','"+_view.txtFecha_Acuerdo_Tres.getText()+"','"+_view.txtAcuerdo_Tres.getText()+"','"+_view.txtFecha_Acuerdo_Cuatro.getText()+"','"+_view.txtAcuerdo_Cuatro.getText()+"'");
-            JOptionPane.showMessageDialog(null, leyenda);
+        if(e.getSource()==_view2.cmdAlta){
+            leyenda=_model.registrarAlta("TbUsuarios"," null, ' " +_view.txtCedula.getText()+" ',' " +_view.txtLugar_Expedicion.getText() +" ',' " +_view.txtNombre_Completo.getText() 
+            +" ',' " +_view.txtProfesion.getText()+" ',' " +_view.txtDireccion_Residencial.getText()+" ',' " +_view.txtCiudad.getText()+" ',' " +_view.txtDepartamento.getText()
+            +" ',' " +_view.txtEmail.getText()+" ',' " +_view.txtTel_Residencial.getText()+" ',' " +_view.txtTel_Trabajo.getText()+" ',' " +_view.txtNum_Factura.getText()+" ',' " +_view.txtTipo_Cartera.getText()
+            +" ',' " +_view.txtFecha_Mora.getText()+" ',' " +_view2.txtDias_Mora.getText()+" ',' " +_view2.txtVal_Mora.getText()+" ',' " +_view2.txtV_Seguro.getText()+" ',' " +_view2.txtInteres_Mora.getText()
+            +" ',' " +_view2.txtSald_Restante.getText()+" ',' " +_view2.txtTipo_Cobro.getText()+" ',' " +_view2.txtFecha_Acuerdo_Uno.getText()+" ',' " +_view2.txtAcuerdo_Uno.getText()
+            +" ',' " +_view2.txtFecha_Acuerdo_Dos.getText()+" ',' " +_view2.txtAcuerdo_Dos.getText()+" ',' " +_view2.txtFecha_Acuerdo_Tres.getText()+" ',' " +_view2.txtAcuerdo_Tres.getText()
+            +" ',' " +_view2.txtFecha_Acuerdo_Cuatro.getText()+" ',' " +_view2.txtFecha_Acuerdo_Cuatro.getText());
+        JOptionPane.showMessageDialog(null, leyenda);
+        
             limpiar();
         }
-    }
+       }
     public void iniciar(){
         _view.setTittle("MVC_Visual");
     }
     public void limpiar(){
        _view.txtCedula.setText(null);
        _view.txtLugar_Expedicion.setText(null);
+       _view.txtNombre_Completo.setAction(null);
+       _view.txtCiudad.setAction(null);
+       _view.txtDepartamento.setAction(null);
+       _view.txtDireccion_Residencial.setAction(null);
+       _view.txtEmail.setAction(null);
+       _view.txtFecha_Mora.setAction(null);
+       _view.txtNum_Factura.setAction(null);
+       _view.txtProfesion.setAction(null);
+       _view.txtTel_Residencial.setAction(null);
+       _view.txtTel_Trabajo.setAction(null);
+       _view.txtTipo_Cartera.setAction(null);
+       
+       _view2.txtDias_Mora.setAction(null);
+       _view2.txtVal_Mora.setAction(null);
+       _view2.txtV_Seguro.setAction(null);
+       _view2.txtInteres_Mora.setAction(null);
+       _view2.txtSald_Restante.setAction(null);
+       _view2.txtTipo_Cobro.setAction(null);
+       _view2.txtFecha_Acuerdo_Uno.setAction(null);
+       _view2.txtAcuerdo_Uno.setAction(null);
+       _view2.txtFecha_Acuerdo_Dos.setAction(null);
+       _view2.txtAcuerdo_Dos.setAction(null);
+       _view2.txtFecha_Acuerdo_Tres.setAction(null);
+       _view2.txtAcuerdo_Tres.setAction(null);
+       _view2.txtFecha_Acuerdo_Cuatro.setAction(null);
+       _view2.txtAcuerdo_Cuatro.setAction(null);
+ 
        _view.txtNombre_Completo.setText(null);
        _view.txtProfesion.setText(null);
        _view.txtDireccion_Residencial.setText(null);
        _view.txtCiudad.setText(null);
        _view.txtDepartamento.setText(null);
        _view.txtEmail.setText(null);
-       _view.txtTel_Residencia.setText(null);
-       _view.txtTel_Trabajo.setText(null)
-       _view.txtNum_Factura.setText(null);
+       _view.txtTel_Residencial.setText(null);
+       _view.txtTel_Trabajo.setText(null);
+       _view.txtNum_Factura.setText(null); 
        _view.txtTipo_Cartera.setText(null);
        _view.txtFecha_Mora.setText(null);
-       _view.txtDias_Mora.setText(null);
-       _view.txtVal_Mora.setText(null);
-       _view.txtV_seguro.setText(null);
-       _view.txtInteres_Mora.setText(null);
-       _view.txtSald_Restante.setText(null);
-       _view.txtTipo_Cobro.setText(null);
-       _view.txtFecha_Acuerdo_Uno.getText(null);
-       _view.txtAcuerdo_Uno.setText(null);
-       _view.txtFecha_Acuerdo_Dos.setText(null);
-       _view.txtAcuerdo_Dos.setText(null);
-       _view.txtFecha_Acuerdo_Tres.setText(null);
-       _view.txtAcuerdo_Tres.setText(null);
-       _view.txtFecha_Acuerdo_Cuatro.setText(null);
-       _view.txtAcuerdo_Cuatro.setText(null);
+       _view2.txtDias_Mora.setText(null);
+       _view2.txtVal_Mora.setText(null);
+       _view2.txtV_Seguro.setText(null);
+       _view2.txtInteres_Mora.setText(null);
+       _view2.txtSald_Restante.setText(null);
+       _view2.txtTipo_Cobro.setText(null);
+       _view2.txtFecha_Acuerdo_Uno.setText(null);
+       _view2.txtAcuerdo_Uno.setText(null);
+       _view2.txtFecha_Acuerdo_Dos.setText(null);
+       _view2.txtAcuerdo_Dos.setText(null);
+       _view2.txtFecha_Acuerdo_Tres.setText(null);
+       _view2.txtAcuerdo_Tres.setText(null);
+       _view2.txtFecha_Acuerdo_Cuatro.setText(null);
+       _view2.txtAcuerdo_Cuatro.setText(null);
     }
 }
--- src/Vista/FrmSeguimiento.java
+++ src/Vista/FrmSeguimiento.java
@@ -23,21 +23,181 @@ public class FrmSeguimiento extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        txtCedula = new javax.swing.JTextField();
+        txtLugar_Expedicion = new javax.swing.JTextField();
+        txtNombre_Completo = new javax.swing.JTextField();
+        txtProfesion = new javax.swing.JTextField();
+        txtDireccion_Residencial = new javax.swing.JTextField();
+        txtCiudad = new javax.swing.JTextField();
+        txtDepartamento = new javax.swing.JTextField();
+        txtEmail = new javax.swing.JTextField();
+        txtTel_Residencial = new javax.swing.JTextField();
+        txtTel_Trabajo = new javax.swing.JTextField();
+        txtTipo_Cartera = new javax.swing.JTextField();
+        txtNum_Factura = new javax.swing.JTextField();
+        txtFecha_Mora = new javax.swing.JTextField();
+        jLabel13 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Cedula");
+
+        jLabel2.setText("Lugar de Expedición");
+
+        jLabel3.setText("Nombre Completo");
+
+        jLabel4.setText("Profesión");
+
+        jLabel5.setText("Dirección Residencial");
+
+        jLabel6.setText("Ciudad");
+
+        jLabel7.setText("Departamento");
+
+        jLabel8.setText("Email");
+
+        jLabel9.setText("Telefono Residencia");
+
+        jLabel10.setText("Telefono Trabajo");
+
+        jLabel11.setText("Número de Factura");
+
+        jLabel12.setText("Tipo cartera");
+
+        jLabel13.setText("Fecha Mora");
+
+        jButton1.setText("Siguiente");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1220, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel11)
+                            .addComponent(jLabel10)
+                            .addComponent(jLabel9)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel13)
+                                .addComponent(jLabel12)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtCedula)
+                            .addComponent(txtLugar_Expedicion)
+                            .addComponent(txtTipo_Cartera, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE)
+                            .addComponent(txtNum_Factura)
+                            .addComponent(txtTel_Trabajo)
+                            .addComponent(txtTel_Residencial)
+                            .addComponent(txtEmail)
+                            .addComponent(txtDepartamento)
+                            .addComponent(txtCiudad)
+                            .addComponent(txtDireccion_Residencial, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtProfesion)
+                            .addComponent(txtNombre_Completo)
+                            .addComponent(txtFecha_Mora))))
+                .addGap(49, 49, 49))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 615, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtLugar_Expedicion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtNombre_Completo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtProfesion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtDireccion_Residencial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(txtCiudad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(txtDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(txtTel_Residencial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel10)
+                    .addComponent(txtTel_Trabajo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel11)
+                    .addComponent(txtNum_Factura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel12)
+                    .addComponent(txtTipo_Cartera, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel13)
+                    .addComponent(txtFecha_Mora, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(21, 21, 21))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        FrmSeguiminto2  frmSeguiminto2=new FrmSeguiminto2();
+        frmSeguiminto2.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -65,6 +225,9 @@ public class FrmSeguimiento extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(FrmSeguimiento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -75,5 +238,36 @@ public class FrmSeguimiento extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    public javax.swing.JTextField txtCedula;
+    public javax.swing.JTextField txtCiudad;
+    public javax.swing.JTextField txtDepartamento;
+    public javax.swing.JTextField txtDireccion_Residencial;
+    public javax.swing.JTextField txtEmail;
+    public javax.swing.JTextField txtFecha_Mora;
+    public javax.swing.JTextField txtLugar_Expedicion;
+    public javax.swing.JTextField txtNombre_Completo;
+    public javax.swing.JTextField txtNum_Factura;
+    public javax.swing.JTextField txtProfesion;
+    public javax.swing.JTextField txtTel_Residencial;
+    public javax.swing.JTextField txtTel_Trabajo;
+    public javax.swing.JTextField txtTipo_Cartera;
     // End of variables declaration//GEN-END:variables
+
+    public void setTittle(String mvc_Visual) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
